6f7c7faac92339a46c0ff222967ab8d96221a041
Nicolas Van Bossuyt <nicovb30@gmail.com>
2017-10-23 20:51:54 +0200

    Add Hello world!

--- WorldOfImagination/WorldOfImagination/Program.cs
+++ WorldOfImagination/WorldOfImagination/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace WorldOfImagination
@@ -11,6 +7,7 @@ namespace WorldOfImagination
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
         }
     }
 }
