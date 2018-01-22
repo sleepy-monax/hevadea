ecb4d58f5de343474914f376c16399b43b4ef25f
Nicolas Van Bossuyt <nicovb30@gmail.com>
2018-01-22 18:46:44 +0100

    Add Selection cursor

--- Maker.Hevadea/Game/Entities/Component/Interaction/Interact.cs
+++ Maker.Hevadea/Game/Entities/Component/Interaction/Interact.cs
@@ -1,5 +1,6 @@
 ﻿using Maker.Hevadea.Game.Items;
 using Maker.Rise.Extension;
+using Maker.Rise.Ressource;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -8,6 +9,13 @@ namespace Maker.Hevadea.Game.Entities.Component.Interaction
 {
     public class Interact : EntityComponent, IDrawableOverlayComponent
     {
+        Sprite cursor;
+
+        public Interact()
+        {
+            cursor = new Sprite(Ressources.tile_icons, 2, new Point(16,16));
+        }
+
         public void Do(Item item)
         {
             var tilePosition = Owner.GetTilePosition();
@@ -29,27 +37,19 @@ namespace Maker.Hevadea.Game.Entities.Component.Interaction
                 }
             }
         }
-      
+
+        Vector2 Sel = Vector2.Zero;
         public void DrawOverlay(SpriteBatch spriteBatch, GameTime gameTime)
         {
             var dir = Owner.Facing.ToPoint();
+            var pos = Owner.GetTilePosition();
 
-
-            var entities = Owner.Level.GetEntitiesOnArea(new Rectangle((int)(Owner.X + Owner.Height * dir.X),
-                (int)(Owner.Y + Owner.Width * dir.Y),
-                Owner.Height, Owner.Width));
-
-            if (entities.Count > 0)
-            {
-                foreach (var e in entities)
-                {
-                    if (e.Components.Has<Interactable>())
-                    {
-                        spriteBatch.FillRectangle(e.Bound, Color.Red * (float)((Math.Sin(gameTime.TotalGameTime.TotalSeconds * 4f) + 1f) / 2f));
-                    }
-                    break;
-                }
-            }
+            var selected = new Point(dir.X + pos.X, dir.Y + pos.Y);
+            var selectionRectangle = new Vector2(selected.X * ConstVal.TileSize, selected.Y * ConstVal.TileSize);
+            Sel = new Vector2((Sel.X*0.5f + selectionRectangle.X*0.5f), (Sel.Y*0.5f + selectionRectangle.Y * 0.5f));
+            var selectedFrame = (gameTime.TotalGameTime.Milliseconds / 100) % 3;
+            var value = ((float)Math.Abs(Math.Sin(gameTime.TotalGameTime.TotalSeconds * 4)) + 1f) / 2;
+            cursor.Draw(spriteBatch, Sel + new Vector2( 16f * (1f - value) / 2), value, Color.White);
         }
     }
 }
