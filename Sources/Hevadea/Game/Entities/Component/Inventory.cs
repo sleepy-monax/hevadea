e259edbdb60c2e48a5d5a7a2ef775d816b71e73d
Nicolas Van Bossuyt <nicovb30@gmail.com>
2018-02-26 22:47:42 +0100

    Remove pickup animation

--- Sources/Hevadea/Game/Entities/Component/Inventory.cs
+++ Sources/Hevadea/Game/Entities/Component/Inventory.cs
@@ -8,10 +8,8 @@ using System.Collections.Generic;
 
 namespace Hevadea.Game.Entities.Component
 {
-    public class Inventory : EntityComponent, IDrawableComponent, IUpdatableComponent, ISaveLoadComponent
+    public class Inventory : EntityComponent, ISaveLoadComponent
     {
-        private readonly EasingManager _pickUpAnimation = new EasingManager();
-
         private Item _lastAdded;
 
         public Inventory(int slotCount)
@@ -21,15 +19,6 @@ namespace Hevadea.Game.Entities.Component
 
         public ItemStorage Content { get; }
         public bool AlowPickUp { get; set; } = false;
-
-        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
-        {
-            if (_lastAdded == null) return;
-            var size = 1f - _pickUpAnimation.GetValue(EasingFunctions.QuadraticEaseOut);
-            _lastAdded.GetSprite().Draw(spriteBatch,
-                new Vector2(Owner.X + Owner.Width / 2f - 8 * size, Owner.Y + Owner.Height / 2 - 24 * size), size,
-                Color.White);
-        }
 
         public void OnGameSave(EntityStorage store)
         {
@@ -45,20 +34,10 @@ namespace Hevadea.Game.Entities.Component
             }
         }
 
-        
-        public void Update(GameTime gameTime)
-        {
-            _pickUpAnimation.Update(gameTime);
-        }
-
         public bool Pickup(Item item)
         {
             if (AlowPickUp && Content.Add(item))
             {
-                _pickUpAnimation.Reset();
-                _pickUpAnimation.Show = true;
-                _pickUpAnimation.Speed = 0.5f;
-
                 _lastAdded = item;
                 return true;
             }
