1df82666c7c35e4358dbc4aa2c563497fc38969d
VAN BOSSUYT Nicolas <nicovb30@gmail.com>
2018-03-06 12:05:34 +0100

    Working on belt

--- Sources/Hevadea/Game/Entities/Mecanics/BeltEntity.cs
+++ Sources/Hevadea/Game/Entities/Mecanics/BeltEntity.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using Hevadea.Framework.Graphic;
+﻿using System;
+using System.Collections.Generic;
 using Hevadea.Framework.Graphic.SpriteAtlas;
 using Hevadea.Game.Entities.Component;
 using Hevadea.Game.Entities.Component.Attributes;
@@ -8,36 +8,36 @@ using Hevadea.Game.Registry;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Hevadea.Game.Entities
+namespace Hevadea.Game.Entities.Mecanics
 {
     public class BeltEntity : Entity
     {
         private static List<Sprite> _sprites;
+        
         public BeltEntity()
         {
             Height = 16;
             Width = 16;
 
             Add(new Breakable());
-            Add(new Dropable(){Items = {new Drop(ITEMS.BELT, 1f, 1, 1)}});
+            Add(new Dropable{ Items = { new Drop(ITEMS.BELT, 1f, 1, 1) } });
 
             if (_sprites == null)
             {
-                _sprites = new List<Sprite>();
-                
-                _sprites.Add(new Sprite(Ressources.TileEntities, new Point(10, 0)));
-                _sprites.Add(new Sprite(Ressources.TileEntities, new Point(10, 1)));
-                _sprites.Add(new Sprite(Ressources.TileEntities, new Point(10, 2)));
-                _sprites.Add(new Sprite(Ressources.TileEntities, new Point(10, 3)));
+                _sprites = new List<Sprite>
+                {
+                    new Sprite(Ressources.TileEntities, new Point(10, 0)),
+                    new Sprite(Ressources.TileEntities, new Point(10, 1)),
+                    new Sprite(Ressources.TileEntities, new Point(10, 2)),
+                    new Sprite(Ressources.TileEntities, new Point(10, 3))
+                };
             }
         }
         
         public override void OnUpdate(GameTime gameTime)
         {
-            var entity = Level.GetEntityOnTile(GetTilePosition());
-            var dir = Facing.ToPoint();
-            
-            foreach (var e in entity)
+            var entities = Level.GetEntityOnTile(GetTilePosition());
+            foreach (var e in entities)
             {
                 e.Get<Pushable>()?.Push(this, Facing, 0.5f);
             }
