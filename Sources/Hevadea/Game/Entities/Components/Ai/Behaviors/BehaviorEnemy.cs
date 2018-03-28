30549515d27e91b6a61cc474123858b09a5f161e
rstevenn <37174144+rstevenn@users.noreply.github.com>
2018-03-28 12:13:58 +0200

    fixe zombie

--- Sources/Hevadea/Game/Entities/Components/Ai/Behaviors/BehaviorEnemy.cs
+++ Sources/Hevadea/Game/Entities/Components/Ai/Behaviors/BehaviorEnemy.cs
@@ -5,18 +5,28 @@ using Microsoft.Xna.Framework;
 
 namespace Hevadea.Game.Entities.Components.Ai.Behaviors
 {
-    public class BehaviorEnemy : BehaviorAnimal
+    public class BehaviorEnemy : IBehavior
     {
         public float AgroRange { get; set; } = 5f;
-        public float FollowRange { get; set; } = 16f;
+        public float FollowRange { get; set; } = 7f;
         public Entity Target { get; private set; } = null;
         public float ChanceToAgro { get; set; } = 0.5f;
-        
-        public new void Update(Agent agent, GameTime gameTime)
+
+        public void IaAborted(Agent agent, AgentAbortReason why)
+        {
+            
+        }
+
+        public void IaFinish(Agent agent)
+        {
+           
+        }
+
+        public void Update(Agent agent, GameTime gameTime)
         {   
             if (Target != null &&
                 Target.Level == agent.Owner.Level &&
-                Mathf.Distance(agent.Owner.Position, Target.Position) > FollowRange)
+                Mathf.Distance(agent.Owner.Position, Target.Position) > FollowRange * 16)
             {
                 agent.Abort(AgentAbortReason.TagetLost);
                 Target = null; 
@@ -31,7 +41,7 @@ namespace Hevadea.Game.Entities.Components.Ai.Behaviors
                     
                     if (game.MainPlayer != null &&
                         game.MainPlayer.Level == agent.Owner.Level &&
-                        Mathf.Distance(game.MainPlayer.Position, agent.Owner.Position) < AgroRange &&
+                        Mathf.Distance(game.MainPlayer.Position, agent.Owner.Position) < AgroRange * 16 &&
                         Rise.Rnd.NextFloat() < ChanceToAgro)
                     {
                         Target = game.MainPlayer;
@@ -40,7 +50,7 @@ namespace Hevadea.Game.Entities.Components.Ai.Behaviors
 
                 if (Target != null)
                 {
-                    agent.MoveTo(Target.GetTilePosition(), MoveSpeed, false, (int)(FollowRange + 4));
+                    agent.MoveTo(Target.GetTilePosition(), 1f, true, (int)(FollowRange + 4));
                 }
                 
             }
