f3223a006cbf35828bbc0500c742d0da3c826e68
VAN BOSSUYT Nicolas <nicovb30@gmail.com>
2018-04-15 20:16:15 +0200

    Trying to optimise rendering

--- Sources/Hevadea/Worlds/Level/Level.LifeCycle.cs
+++ Sources/Hevadea/Worlds/Level/Level.LifeCycle.cs
@@ -56,16 +56,16 @@ namespace Hevadea.Worlds
         {
             var entitiesOnScreen = new List<Entity>();
             var focusEntity = new Point((int) camera.X / Constant.TileSize, (int) camera.Y / Constant.TileSize);
-            var dist = new Point(camera.GetWidth() / 2 / Constant.TileSize + 8,
-                camera.GetHeight() / 2 / Constant.TileSize + 8);
+            var dist = new Point(camera.GetWidth() / 2 / Constant.TileSize,
+                camera.GetHeight() / 2 / Constant.TileSize);
 
             var state = new LevelRenderState
             {
                 Begin = new Point(Math.Max(0, focusEntity.X - dist.X),
-                    Math.Max(0, focusEntity.Y - dist.Y)),
+                    Math.Max(0, focusEntity.Y - dist.Y - 1)),
 
-                End = new Point(Math.Min(Width, focusEntity.X + dist.X),
-                    Math.Min(Height, focusEntity.Y + dist.Y))
+                End = new Point(Math.Min(Width, focusEntity.X + dist.X + 1),
+                    Math.Min(Height, focusEntity.Y + dist.Y) + 6)
             };
 
             for (var tx = state.Begin.X; tx < state.End.X; tx++)
