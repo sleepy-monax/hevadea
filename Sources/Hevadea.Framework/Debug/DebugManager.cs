ed0c0fe381cbb52553262dcc1027dad4b46fd5af
Nicolas Van Bossuyt <nicovb30@gmail.com>
2018-04-16 22:26:46 +0200

    Improve debug overlayer

--- Sources/Hevadea.Framework/Debug/DebugManager.cs
+++ Sources/Hevadea.Framework/Debug/DebugManager.cs
@@ -23,14 +23,16 @@ namespace Hevadea.Framework.Debug
         public void Draw(GameTime gameTime)
         {
             _spriteBatch.Begin();
-            
-            _spriteBatch.DrawString(Rise.Ui.DebugFont, 
-            $@"Hevadea
+
+            var text = $@"Hevadea
 Update: {Rise.MonoGame.UpdateTime}
 Draw:   {Rise.MonoGame.DrawTime}
 Running on platform: '{Rise.Platform.GetPlatformName()}'
 Screen: {Rise.Graphic.GetWidth()}, {Rise.Graphic.GetHeight()} 
-{Rise.Scene?.GetCurrentScene()?.GetDebugInfo() ?? ""}", new Vector2(16, 16), Color.White);
+{Rise.Scene?.GetCurrentScene()?.GetDebugInfo() ?? ""}";
+
+            _spriteBatch.DrawString(Rise.Ui.DebugFont, text, new Vector2(16, 16 + 1), Color.Black);
+            _spriteBatch.DrawString(Rise.Ui.DebugFont, text, new Vector2(16, 16), Color.White);
             Rise.Pointing.DrawDebug(_spriteBatch);
             _spriteBatch.End();
         }
