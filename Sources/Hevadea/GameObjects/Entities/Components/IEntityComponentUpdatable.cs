808917fda92ad209adb49f52a3346b4882b152aa
Nicolas Van Bossuyt <nicovb30@gmail.com>
2018-05-11 11:41:40 +0200

    Cleanup repo

--- Sources/Hevadea/GameObjects/Entities/Components/IEntityComponentUpdatable.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace Hevadea.GameObjects.Entities.Components
-{
-    public interface IEntityComponentUpdatable
-    {
-        void Update(GameTime gameTime);
-    }
-}
