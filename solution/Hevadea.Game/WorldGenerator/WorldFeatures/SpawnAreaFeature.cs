f8fe51882e6583844ed9d3a374629c2edda22556
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2019-07-20 09:18:45 +0200

    Generator: making the spawn area more natural

--- solution/Hevadea.Game/WorldGenerator/WorldFeatures/SpawnAreaFeature.cs
+++ solution/Hevadea.Game/WorldGenerator/WorldFeatures/SpawnAreaFeature.cs
@@ -1,20 +1,33 @@
 ﻿using Hevadea.Registry;
 using Hevadea.Worlds;
+using Hevadea.Framework.Extension;
 using Microsoft.Xna.Framework;
 
 namespace Hevadea.WorldGenerator.WorldFeatures
 {
     public class SpawnAreaFeature : WorldFeature
     {
+        public const int SPAWN_AREA_SIZE = 3;
         public string SpawnLevelName { get; set; } = "overworld";
 
         public override void Apply(Generator gen, World world)
         {
             var center = new Point(gen.Size / 2, gen.Size / 2);
             world.PlayerSpawnLevel = SpawnLevelName;
+
             var spawnLevel = world.GetLevel(SpawnLevelName);
-            spawnLevel.ClearEntitiesAt(center.X - 5, center.Y - 5, 10, 10);
-            spawnLevel.FillRectangle(center.X - 1, center.Y - 1, 3, 3, TILES.DIRT);
+            spawnLevel.ClearEntitiesAt(center.X - SPAWN_AREA_SIZE / 2, center.Y - SPAWN_AREA_SIZE / 2, SPAWN_AREA_SIZE, SPAWN_AREA_SIZE);
+            spawnLevel.FillRectangle(center.X - SPAWN_AREA_SIZE / 2, center.Y - SPAWN_AREA_SIZE / 2, SPAWN_AREA_SIZE, SPAWN_AREA_SIZE, TILES.GRASS);
+
+            for (int i = 0; i < 25; i++)
+            {
+                var xx = center.X + (int)(SPAWN_AREA_SIZE * gen.Random.NextFloatRange(1f));
+                var yy = center.Y + (int)(SPAWN_AREA_SIZE * gen.Random.NextFloatRange(1f));
+
+                spawnLevel.SetTile(xx, yy, TILES.GRASS);
+            }
+
+            spawnLevel.AddEntityAt(ENTITIES.TREE, new Coordinates(center.X + gen.Random.Pick(-1, 1), center.Y + gen.Random.Pick(-1, 1)));
         }
 
         public override string GetName()
